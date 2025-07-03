a4f4f5c1abb989e754dded785cead70f8367cd64
OzWhy114 <2777182907@qq.com>
2025-07-03 19:44:04 +0800

    Site updated: 2025-07-03 19:44:04

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1743598025287";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1751530436868";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
